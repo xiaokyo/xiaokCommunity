24a352aeac23529065e167b8c4a7bac28399fa86
xiaokyo <904656778@qq.com>
2019-09-27 13:40:10 +0800

    add pm2 deploy

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [{
+    name: 'xiaokCommunity',
+    script: 'dist/server/server.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    // args: 'one two',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }]
+};
